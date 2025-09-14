3bc6190a457fd86e01f963ca3c00417b6781a7a7
stphn <sgoeuriot@gmail.com>
2025-09-14 12:08:36 +0200

    updated docs

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -96,22 +96,28 @@ tap_dance_action_t tap_dance_actions[] = {
 /* ============================== Keymaps ================================ */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY (base)
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |  Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |  A   |  S   |  D   |  F   |  G   |  H   |  J   |  K   |  L   |  ;   |  '   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /   |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  | NUM  |  󱁐   |  󰿄   | SYM  | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
+/* QWERTY 
+ * ,-----------------------------------------------------------------------------------------------------------------------.
+ * |   Tab   |    Q    |    W    |    E    |    R    |    T    |    Y    |    U    |    I    |    O    |    P    |  Bksp   |
+ * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
+ * | Ctr/Esc |    A    |    S    |    D    |    F    |    G    |    H    |    J    |    K    |    L    |    ;    |   '     |
+ * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
+ * | Shift/( |    Z    |    X    |   C/⎘   |    C    |   V/⎘   |    V    |    B    |    ,    |    .    |   / ?   | Shift/) |
+ * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
+ * |    [    |   Alt   |   HYPR  |   NUM   |   LGUI  |   Space |  Ent/⌫  |   NAV   |   SYM   |   RGUI  |   Alt   |    ]    |
+ * `-----------------------------------------------------------------------------------------------------------------------'
+ * Notes:
+ *  - Tap-dance:
+ *      ; → ; / :      ' → ' / "
+ *      C → C / ⌘C     V → V / ⌘V
+ *      / → / / ?
+ *  - Outer brackets [ ] mirror Colemak layer for symmetry.
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H, KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, NUM,     SFT_T(KC_SPC), KC_ENT,  SYM,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_TAB,     KC_Q,   KC_W,       KC_E,          KC_R,   KC_T,           KC_Y,          KC_U,   KC_I,    KC_O      , KC_P            , KC_BSPC,
+    CTRL_ESC,   KC_A,   KC_S,       KC_D,          KC_F,   KC_G,           KC_H,          KC_J,   KC_K,    KC_L,     KC_SCLN          , TD(TD_QUOT_DQUOT),
+    SC_LSPO,    KC_Z,   KC_X,       TD(TD_C_COPY), KC_C,   TD(TD_V_PASTE), KC_V,          KC_B,   KC_COMM, KC_DOT    , TD(TD_SLSH_QUES), SC_RSPC,
+    KC_LBRC,    KC_LALT,HYPR(KC_NO),NUM,           KC_LGUI,SFT_T(KC_SPC),  LT(0, KC_ENT), NAV,    SYM,     KC_RGUI   , KC_RALT         , KC_RBRC
 ),
 
 /* Colemak (base)
